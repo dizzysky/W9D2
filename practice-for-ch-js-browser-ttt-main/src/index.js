dbc4b8f43f5084013ea48df95a275dfb0d031778
elevenstand7 <elevenstand7@gmail.com>
2023-08-22 15:09:42 -0400

    finished the board

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,10 @@
-Add your import statements for View and Game here
+//Add your import statements for View and Game here
+import View from "./ttt-view";
+import Game from "../ttt_node/game";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const game = new Game();
+  const container = document.querySelector(".ttt");
+  const view = new View(game, container);
 });
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,18 +1,31 @@
 class View {
   constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard();
   }
-  
+
   setupBoard() {
+    const ul = document.createElement("ul");
+    for(let row = 0;row<3;row++){
+      for(let col = 0;col<3;col++){
+        let li = document.createElement("li");
+        li.dataset.pos = JSON.stringify([row, col]);
+        ul.append(li);
+      }
+    }
+    this.el.append(ul);
   }
-  
+
   handleClick(e) {
   }
 
   makeMove(square) {
   }
-  
+
   handleGameOver() {
   }
 }
 
+console.log('view');
 export default View;
